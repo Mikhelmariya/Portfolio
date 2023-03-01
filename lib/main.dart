908a31aaf3591fe757b55ceb355a9a57084b8085
Mikhelmariya <mikhela65@gmail.com>
2023-03-01 20:30:41 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Portfolio());
+}
+
+class Portfolio extends StatelessWidget {
+  const Portfolio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
